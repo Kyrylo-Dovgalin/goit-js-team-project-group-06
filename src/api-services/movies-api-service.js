a4932f8a80363f479254b71f7d159b838546d1cb
Oleg-Strong <superstrong0@gmail.com>
2022-12-15 11:32:36 +0200

    add movies-api and moviesMarkup

---
+++ src/api-services/movies-api-service.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+export { getPopularMovies, getGanres };
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = 'f983fc840eb543faba07dcbe6db19b0b';
+const getPopularMovies = async () => {
+  return await axios.get(`${BASE_URL}trending/movie/day?api_key=${API_KEY}`);
+};
+// Запрос на массив объектов жанров, его нужно отфильтровать относительно id которые приходят с запроса getPopularMovies в свойстве genre_ids. Ну и зарэндерить в разметку
+const getGanres = async () => {
+  return await axios.get(`${BASE_URL}genre/movie/list?api_key=${API_KEY}`);
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './js/header';
 import './js/search';
-import './js/movies';
+import { createMoviesMarkup } from './js/movies';
+import { getPopularMovies, getGanres } from './api-services/movies-api-service';
+getPopularMovies().then(({ data }) => createMoviesMarkup(data));
+
+getGanres().then(({ data }) => console.log(data));
