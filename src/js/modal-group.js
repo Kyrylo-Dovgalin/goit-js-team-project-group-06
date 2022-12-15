da13064841ac9f4385edac74015e2c04ec86b8e5
Viktoriia Moskalenko <vika01mos@gmail.com>
2022-12-15 17:26:44 +0200

    fix modal_group

--- src/js/modal-group.js
+++ src/js/modal-group.js
@@ -1,26 +1,18 @@
 import SimpleSlider from 'simpleslider-js';
 import 'simpleslider-js/dist/simpleslider.min.css';
-// import SimpleLightbox from 'simplelightbox';
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-    gallery: document.querySelector('.gallery'),
-  };
+const refs = {
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+};
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-    if (!refs.modal.classList.contains('is-hidden')) {
-      new SimpleSlider('.simple-slider-first');
-      //   let gallery = new SimpleLightbox('.gallery a');
-      //   gallery.on('show.simplelightbox', function () {
-      //     // Do something…
-      //   });
-    }
+function toggleModal() {
+  refs.modal.classList.toggle('is-hidden');
+  if (!refs.modal.classList.contains('is-hidden')) {
+    new SimpleSlider('.simple-slider-first');
   }
-})();
+}
