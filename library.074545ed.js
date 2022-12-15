3c62feead991da0ffcc1185a3eaecd8a893bf983
Kyrylo-Dovgalin <104022593+Kyrylo-Dovgalin@users.noreply.github.com>
2022-12-15 18:43:44 +0000

    Deploying to gh-pages from @ Kyrylo-Dovgalin/goit-js-team-project-group-06@d1dcef80bc9fa133aac4bd3cb00908149db67a8d 🚀

---
+++ library.074545ed.js
@@ -0,0 +1,2 @@
+!function(){var d=document.querySelector(".mask");window.addEventListener("load",(function(){d.classList.add("hide")}))}();
+//# sourceMappingURL=library.074545ed.js.map
---
+++ library.091f5c4d.js
@@ -0,0 +1,2 @@
+const d=document.querySelector(".mask");window.addEventListener("load",(()=>{d.classList.add("hide")}));
+//# sourceMappingURL=library.091f5c4d.js.map
