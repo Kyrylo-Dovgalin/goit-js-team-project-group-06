6f06d8559bb9a732318d3790a1746856ba995647
Viktoriia Moskalenko <vika01mos@gmail.com>
2022-12-16 20:49:43 +0200

    modal-group

--- src/js/modal-group.js
+++ src/js/modal-group.js
@@ -1,6 +1,3 @@
-import SimpleSlider from 'simpleslider-js';
-//import 'simpleslider-js/dist/simpleslider.min.css';
-
 const refs = {
   openModalBtn: document.querySelector('[data-modal-open]'),
   closeModalBtn: document.querySelector('[data-modal-close]'),
@@ -12,7 +9,4 @@ refs.closeModalBtn.addEventListener('click', toggleModal);
 
 function toggleModal() {
   refs.modal.classList.toggle('is-hidden');
-  if (!refs.modal.classList.contains('is-hidden')) {
-    new SimpleSlider('.simple-slider-first');
-  }
 }
