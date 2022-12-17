fa49c72cb190687260b8b1ddc50114eb99cc9ddf
Viktoriia Moskalenko <vika01mos@gmail.com>
2022-12-17 15:22:29 +0200

    modal-group

--- src/js/modal-group.js
+++ src/js/modal-group.js
@@ -1,12 +1,26 @@
-const refs = {
+import { refs } from '../js/modalMoviesCard';
+const reference = {
   openModalBtn: document.querySelector('[data-modal-open]'),
   closeModalBtn: document.querySelector('[data-modal-close]'),
   modal: document.querySelector('[data-modal]'),
 };
 
-refs.openModalBtn.addEventListener('click', toggleModal);
-refs.closeModalBtn.addEventListener('click', toggleModal);
+reference.openModalBtn.addEventListener('click', openModal);
+reference.closeModalBtn.addEventListener('click', closeModal);
 
-function toggleModal() {
-  refs.modal.classList.toggle('is-hidden');
+// function toggleModal() {
+//   reference.modal.classList.toggle('is-hidden');
+//   if (!reference.modal.classList.contains('is-hidden')) {
+//     refs.classlist.remove('backdrop-movie');
+//   }
+// }
+function openModal() {
+  reference.modal.classList.remove('is-hidden');
+  refs.backdrop.remove('is-hidden');
+  refs.backdrop.remove('.backdrop-movie');
+  console.log(reference.modal);
+}
+
+function closeModal() {
+  reference.modal.classList.add('is-hidden');
 }
