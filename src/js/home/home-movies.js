99d132236593d97861c1c00597147cc901817211
SvetlanaPopenko <spopenko02@gmail.com>
2022-12-18 00:44:19 +0200

    Update home-movies.js

--- src/js/home/home-movies.js
+++ src/js/home/home-movies.js
@@ -1,9 +1,10 @@
 import {
   getPopularMovies,
   getGanres,
+  onKeyWord,
 } from '../../api-services/movies-api-service';
 import { createPagi } from '../pagination';
-export { galleryConteiner, createMoviesMarkup };
+export { galleryConteiner, createMoviesMarkup, createMoviesMarkupKey };
 
 const galleryConteiner = document.querySelector('.movies__list');
 let page = 1;
@@ -69,3 +70,46 @@ function replaceIdtoGener(arrGener, arrGenerId) {
     }
   });
 }
+
+async function createMoviesMarkupKey() {
+  const response = await onKeyWord();
+  const results = response.data.results;
+  const arrGenerId = response.data.results.map(item => item.genre_ids);
+
+  const genreResponse = await getGanres();
+  const arrGener = genreResponse.data.genres;
+
+  replaceIdtoGener(arrGener, arrGenerId);
+
+  const markup = results
+    .map(
+      ({
+        poster_path,
+        title,
+        release_date,
+        genre_ids,
+      }) => `<li class="movies__card">
+    <img
+      class="movies__card-photo"
+      src="https://image.tmdb.org/t/p/w500${poster_path}"
+      alt="${title}"
+      loading="lazy"
+      width="395px"
+      height="354px"
+    />
+    <h2 class="movies__card-title">${title}</h2>
+    <p class="movies__card-genres">${genre_ids.join(
+      ', '
+    )} | ${release_date.slice(0, 4)}</p>
+  </li>`
+    )
+    .join('');
+  galleryConteiner.insertAdjacentHTML('beforeend', markup);
+}
+onKeyWord()
+  .then(({ data }) => {
+    createMoviesMarkupKey(data);
+    const totalRes = data.total_results;
+    createPagi(totalRes);
+  })
+  .catch(error => console.log(error));
