27406577b3037b06efbcd564276fc4cabb16e12f
Viktoriia Moskalenko <vika01mos@gmail.com>
2022-12-17 16:02:26 +0200

    fix modal-group

--- src/js/modal-group.js
+++ src/js/modal-group.js
@@ -1,26 +1,29 @@
-import { refs } from '../js/modalMoviesCard';
-const reference = {
+const refs = {
   openModalBtn: document.querySelector('[data-modal-open]'),
   closeModalBtn: document.querySelector('[data-modal-close]'),
-  modal: document.querySelector('[data-modal]'),
+  modal: document.querySelector('[data-modal-group]'),
 };
 
-reference.openModalBtn.addEventListener('click', openModal);
-reference.closeModalBtn.addEventListener('click', closeModal);
+refs.openModalBtn.addEventListener('click', openModal);
+refs.closeModalBtn.addEventListener('click', closeModal);
 
 // function toggleModal() {
 //   reference.modal.classList.toggle('is-hidden');
-//   if (!reference.modal.classList.contains('is-hidden')) {
-//     refs.classlist.remove('backdrop-movie');
-//   }
 // }
+
 function openModal() {
-  reference.modal.classList.remove('is-hidden');
-  refs.backdrop.remove('is-hidden');
-  refs.backdrop.remove('.backdrop-movie');
-  console.log(reference.modal);
+  refs.modal.classList.remove('is-hidden');
+  document.addEventListener('keydown', event => closeModalEscape(event));
 }
 
 function closeModal() {
-  reference.modal.classList.add('is-hidden');
+  refs.modal.classList.add('is-hidden');
+  document.removeEventListener('keydown', event => closeModalEscape(event));
+}
+
+function closeModalEscape(event) {
+  if (event.key !== 'Escape') {
+    return;
+  }
+  closeModal();
 }
