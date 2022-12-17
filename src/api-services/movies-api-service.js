4f5199cfd45096b12b1ab19113bf71b5b07cb38d
SvetlanaPopenko <spopenko02@gmail.com>
2022-12-17 23:44:07 +0200

    Update movies-api-service.js

--- src/api-services/movies-api-service.js
+++ src/api-services/movies-api-service.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
-export { getPopularMovies, getGanres, BASE_URL, API_KEY};
+export { getPopularMovies, getGanres, BASE_URL, API_KEY, onKeyWord};
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = 'f983fc840eb543faba07dcbe6db19b0b';
 const getPopularMovies = async (page=1) => {
   return await axios.get(`${BASE_URL}trending/movie/day?api_key=${API_KEY}&page=${page}`);
+};
+const onKeyWord = async (searchQuery, page=1) => {
+  return await axios.get(`${BASE_URL}search/movie?api_key=${API_KEY}&query=${searchQuery}&page=${page}`);
 };
 // Запрос на массив объектов жанров, его нужно отфильтровать относительно id которые приходят с запроса getPopularMovies в свойстве genre_ids. Ну и зарэндерить в разметку
 const getGanres = async () => {
