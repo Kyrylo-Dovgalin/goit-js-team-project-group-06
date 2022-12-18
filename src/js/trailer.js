511e8de7abfb1544655c8aa4946ff440a8fda618
SixtyByte <valentinvodopyan@ukr.net>
2022-12-18 17:52:00 +0200

    1

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
+let idTrailer;
 const trailerButton = document.querySelector(`.button-open-trailer`);
 const API_KEY = `f983fc840eb543faba07dcbe6db19b0b`;
 const base_url = `https://api.themoviedb.org/3/movie/`;
@@ -10,15 +10,41 @@ const fetchTrailer = async id => {
 trailerButton.addEventListener(`click`, clickTrailer);
 function clickTrailer(event) {
   event.preventDefault();
-  fetchTrailer(id, 1).then(data => {
+  fetchTrailer(idTrailer, 1).then(data => {
     console.log(data.data.results[0].key),
+    console.log(data.data.results.length),
       window.open(
         `https://www.youtube.com/watch?v=${data.data.results[0].key}`,
         '_blank'
       );
-    if (data.data.results.length == 0) {
+    if (data.data.results.length===0) { console.log(data.data.results.length)
       Notify.failure(`Oops, no trailer for this film`);
-      trailerButton.disabled = true;
+      
     }
   });
+}
+const cardMovieSearch = document.querySelector('.movies__list');
+cardMovieSearch.addEventListener('click', searchIdMovieTrailer);
+
+
+
+async function searchIdMovieTrailer(e) {
+  if (e.target.nodeName === 'P') {
+    const idMovie = e.target.parentElement.parentElement.id;
+    idTrailer=idMovie
+  }
+  if (e.target.nodeName === 'LI') {
+    const idMovie = e.target.id;
+    idTrailer= idMovie
+  }
+  if (
+    e.target.nodeName === 'DIV' ||
+    e.target.nodeName === 'IMG' ||
+    e.target.nodeName === 'H2'
+  ) {
+    const idMovie = e.target.parentElement.id;
+    idTrailer= idMovie;
+    
+    
+  }
 }
