112b3fb1e8ba1163e1e7df32f7a9d5a245c67612
SvetlanaPopenko <spopenko02@gmail.com>
2022-12-18 12:19:14 +0200

    Update search.js

--- src/js/home/search.js
+++ src/js/home/search.js
@@ -1,22 +1,19 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { onKeyWord } from '../../api-services/movies-api-service';
 import { createMoviesMarkupKey, galleryConteiner } from './home-movies';
-import { createPagi } from '../pagination';
-export { searchQuery };
+import { createPagiKey } from '../pagination';
 
 const refs = {
   searchForm: document.querySelector('.header__form'),
-  };
+};
 
 let searchQuery = '';
-let page = 1;
 
 refs.searchForm.addEventListener('submit', onSearch);
 
 async function onSearch(evt) {
   evt.preventDefault();
   searchQuery = evt.currentTarget.elements.searchQuery.value;
-  console.dir(searchQuery);
   page = 1;
   galleryConteiner.innerHTML = '';
 
@@ -25,14 +22,12 @@ async function onSearch(evt) {
     return;
   }
 
-
   await onKeyWord(searchQuery, page)
     .then(({ data }) => {
-      console.dir({ data })
-    createMoviesMarkupKey(searchQuery, page);
-    const totalRes = data.total_results;
-    createPagi(totalRes);
-  })
+      createMoviesMarkupKey(searchQuery, page);
+      const totalRes = data.total_results;
+      createPagiKey(searchQuery, totalRes);
+    })
     .catch(error => console.log(error))
     .finally(() => {
       refs.searchForm.reset();
