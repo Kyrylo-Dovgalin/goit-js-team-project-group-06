9246095e2334a47f38f52be80c6045b95fddc7cb
SvetlanaPopenko <spopenko02@gmail.com>
2022-12-18 12:19:24 +0200

    Update pagination.js

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,8 +1,8 @@
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
 import { getPopularMovies } from '../api-services/movies-api-service';
-import { galleryConteiner, createMoviesMarkup } from './home/home-movies';
-export { createPagi };
+import { galleryConteiner, createMoviesMarkup, createMoviesMarkupKey } from './home/home-movies';
+export { createPagi, createPagiKey };
 function createPagi(total) {
   const pagination = new Pagination(
     document.getElementById('tui-pagination-container'),
@@ -19,5 +19,25 @@ function createPagi(total) {
     galleryConteiner.innerHTML = '';
     const currentPage = event.page;
     createMoviesMarkup(currentPage);
+  });
+}
+
+function createPagiKey(searchQuery, total) {
+  const pagination = new Pagination(
+    document.getElementById('tui-pagination-container'),
+    {
+      totalItems: total,
+      itemsPerPage: 20,
+      visiblePages: 5,
+      centerAlign: true,
+      firstItemClassName: 'tui-first-child',
+      lastItemClassName: 'tui-last-child',
+    }
+  );
+  pagination.on('afterMove', event => {
+    galleryConteiner.innerHTML = '';
+    const currentPage = event.page;
+    
+    createMoviesMarkupKey(searchQuery,currentPage);
   });
 }
