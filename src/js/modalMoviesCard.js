6f4e8afcb37ebf65707f97239eb5daad830ecfd4
Vitaliy Meshechek <meshechek.vitaliy@ukr.net>
2022-12-19 20:08:41 +0200

    change render the modal window

--- src/js/modalMoviesCard.js
+++ src/js/modalMoviesCard.js
@@ -27,6 +27,7 @@ function openModal(event) {
     refs.backdrop.classList.add('backdrop-movie');
     refs.movieContainer.classList.remove('is-hidden');
   }, 500);
+
 }
 
 function closeModal(event) {
@@ -54,38 +55,10 @@ function closeModalEscape(event) {
   closeModal();
 }
 
-// const localStorageMovies = (key, value) => {
-//   try {
-//     if (typeof value === 'string') {
-//       localStorage.setItem(key, value);
-//     } else {
-//       localStorage.setItem(key, JSON.stringify(value));
-//     }
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
-// const getFromLocalStorage = key => {
-//   try {
-//     return JSON.parse(localStorage.getItem(key));
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
-// const removeLocalFromStorage = key => {
-//   try {
-//     localStorage.removeItem(key);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
 // Добавляем слушателя событий на список с карточками фильмов
 
-const cardMovie = document.querySelector('.movies__list');
-cardMovie.addEventListener('click', searchIdforMovie);
+// const cardMovie = document.querySelector('.movies__list');
+refs.openModal.addEventListener('click', searchIdforMovie);
 
 //Получаем данные по ID фильма и после того как приходят данные по запросу рендерим данные в модалку
 
@@ -93,10 +66,12 @@ async function searchIdforMovie(e) {
   if (e.target.nodeName === 'P') {
     const idMovie = e.target.parentElement.parentElement.id;
     const response = await fetchById(idMovie);
+    createMarkupMovieCardInModal(response);
   }
   if (e.target.nodeName === 'LI') {
     const idMovie = e.target.id;
     const response = await fetchById(idMovie);
+    createMarkupMovieCardInModal(response);
   }
   if (
     e.target.nodeName === 'DIV' ||
@@ -105,87 +80,90 @@ async function searchIdforMovie(e) {
   ) {
     const idMovie = e.target.parentElement.id;
     const response = await fetchById(idMovie);
-    console.log(response);
+    createMarkupMovieCardInModal(response);
+
   }
 }
 
-function createMarkupMovieCardInModal(data) {
-  const dataMovie = data.reduce(({ poster_path, original_title, title, vote_average, vote_count, genres, overview, popularity, }) => {
-    return acc + `<div class="backdrop-movie is-hidden" data-modal>
-            <div class="modal-movie">
-              <button class="button-close" data-modal-close>
-                <svg class="button-close_icon">
-                  <use href="./images/Icons/symbol-defs.svg#icon-close"></use>
-                </svg>
-              </button>
-              <div class="movie-card">
-                <div class="movie-card_request">
-                  <div class="movie-card_img-cover">
-                    <img
-                    class="movie-card_photo"
-                    src="${poster_path}"
-                    alt="${title}"
-                  />
-                    <button class="button-open-trailer"></button>
-                  </div>
-                </div>
-                <div class="movie-description">
-                  <h2 class="movie-title">${title}</h2>
-                  <table class="movie-table">
-                    <tbody>
-                      <tr class="movie-table_row">
-                        <td>
-                          <p class="movie-table_title">Vote/Votes</p>
-                        </td>
-                        <td>
-                          <p>
-                            <span movie-table_vote>${vote_average} / ${vote_count}</span>
-                          </p>
-                        </td>
-                      </tr>
-                      <tr class="movie-table_row">
-                        <td>
-                          <p class="movie-table_title">Popularity</p>
-                        </td>
-                        <td>
-                          <p>${popularity}</p>
-                        </td>
-                      </tr>
-                      <tr class="movie-table_row">
-                        <td>
-                          <p class="movie-table_title">Original Title</p>
-                        </td>
-                        <td>
-                          <p>${original_title}</p>
-                        </td>
-                      </tr>
-                      <tr class="movie-table_row">
-                        <td>
-                          <p class="movie-table_title">Genre</p>
-                        </td>
-                        <td>
-                          <p>${Object.values(genres)}</p>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                  <div class="movie-about_container">
-                  <h3 class="movie-about">About</h3>
-                  <p class="movie-about_text">${overview}</p>
-                </div>
-                  <ul class="movie-list">
-                    <li class="movie-item">
-                      <button type="button" class="movie-item_button">ADD TO WATHED</button>
-                    </li>
-                    <li class="movie-item">
-                      <button type="button" class="movie-item_button">ADD TO QUEUE</button>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>`
-}, ", ")
+function createMarkupMovieCardInModal({poster_path,
+  original_title,
+  title,
+  vote_average,
+  vote_count,
+  genres,
+  overview,
+  popularity,
+  id,}) {
 
-refs.movieContainer.insertAdjacentHTML("beforeend", dataMovie);
+  const movieGenres = genres.map(({ name }) => name).join(", ");
+
+  const markup = `<div class="movie-card">
+  <div class="movie-card_request">
+    <div class="movie-card_img-cover">
+      <img
+      class="movie-card_photo"
+      src=https://image.tmdb.org/t/p/original${poster_path}
+      alt="${title}"
+    />
+      <button class="button-open-trailer"></button>
+    </div>
+  </div>
+  <div class="movie-description">
+    <h2 class="movie-title">${title}</h2>
+    <table class="movie-table">
+      <tbody>
+        <tr class="movie-table_row">
+          <td>
+            <p class="movie-table_title">Vote/Votes</p>
+          </td>
+          <td>
+            <p>
+              <span movie-table_vote>${vote_average} / ${vote_count}</span>
+            </p>
+          </td>
+        </tr>
+        <tr class="movie-table_row">
+          <td>
+            <p class="movie-table_title">Popularity</p>
+          </td>
+          <td>
+            <p>${popularity}</p>
+          </td>
+        </tr>
+        <tr class="movie-table_row">
+          <td>
+            <p class="movie-table_title">Original Title</p>
+          </td>
+          <td>
+            <p>${original_title}</p>
+          </td>
+        </tr>
+        <tr class="movie-table_row">
+          <td>
+            <p class="movie-table_title">Genre</p>
+          </td>
+          <td>
+            <p>${movieGenres}</p>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="movie-about_container">
+    <h3 class="movie-about">About</h3>
+    <p class="movie-about_text">${overview}</p>
+  </div>
+    <ul class="movie-list">
+      <li class="movie-item">
+        <button type="button" class="movie-item_button" data-id=${id}>ADD TO WATHED</button>
+      </li>
+      <li class="movie-item">
+        <button type="button" class="movie-item_button" data-id=${id}>ADD TO QUEUE</button>
+       </li>
+    </ul>
+  </div>
+</div>`;
+
+refs.movieCard.innerHTML = markup;
 }
+
+
