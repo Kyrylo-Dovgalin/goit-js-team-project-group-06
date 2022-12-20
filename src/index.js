35897845432c8ccc3dea6512521056ae8ad78774
Kyrylo Dovgalin <kyrylo.dovgalin@gmail.com>
2022-12-20 21:42:30 +0200

    del unnec

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import './js/signIn/sing-in';
-import './js/home/home-header';
 import './js/home/home-movies';
 import './js/loader';
-import './js/footer';
 import './js/modalMoviesCard';
 import './js/modal-group';
 import './js/pagination';
 import './js/trailer';
 import './js/home/search';
-
--- src/library.js
+++ src/library.js
@@ -1,8 +1,6 @@
 import './js/signIn/sing-in';
 import './js/library/library-header';
-import './js/library/library-movies';
 import './js/loader';
-import './js/footer';
 import './js/modalMoviesCard';
 import './js/modal-group';
 import './js/trailer';
